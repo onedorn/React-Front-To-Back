636cb708d5ef1435a4938987fe06f69013569880
Volodymyr Syvoraksha <vpsyvoraksha@gmail.com>
2019-09-16 00:41:38 +0300

    Search component

--- github-finder/src/App.js
+++ github-finder/src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import Navbar from "./components/layouts/Navbar";
 import User from "./components/users/User";
 import axios from "axios";
+import Search from "./components/users/Search";
 
 class App extends Component {
   state = {
@@ -12,13 +13,17 @@ class App extends Component {
 
   componentDidMount() {
     this.setState({ loading: true });
-
-    axios.get("https://api.github.com/users").then(res =>
-      this.setState({
-        users: res.data,
-        loading: false
-      })
-    );
+    
+    axios
+      .get(
+        `https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+      )
+      .then(res =>
+        this.setState({
+          users: res.data,
+          loading: false
+        })
+      );
   }
 
   render() {
@@ -26,6 +31,7 @@ class App extends Component {
       <div>
         <Navbar />
         <div className="container">
+          <Search />
           <User loading={this.state.loading} users={this.state.users} />
         </div>
       </div>
