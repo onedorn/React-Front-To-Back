1a98a0f04b948454d5e8043dad7324e785d96087
Volodymyr Syvoraksha <Volodymyr_Syvoraksha@epam.com>
2019-09-15 21:20:18 +0300

    Init project

---
+++ github-finder/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+export class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1 className='text-danger'>Hello from App js file</h1>
+      </div>
+    )
+  }
+}
+
+export default App
