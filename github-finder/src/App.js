b170569e911d81d65fa8a47038076c34203e3bfd
Volodymyr Syvoraksha <vpsyvoraksha@gmail.com>
2019-09-16 11:36:24 +0300

    Alert component

--- github-finder/src/App.js
+++ github-finder/src/App.js
@@ -4,26 +4,39 @@ import Navbar from "./components/layouts/Navbar";
 import User from "./components/users/User";
 import axios from "axios";
 import Search from "./components/users/Search";
+import Alert from "./components/layouts/Alert";
 
 class App extends Component {
   state = {
     users: [],
-    loading: false
+    loading: false,
+    alert: null
   };
 
-  componentDidMount() {
+  searchUsers = text => {
     this.setState({ loading: true });
-    
     axios
       .get(
-        `https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+        `https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
       )
       .then(res =>
         this.setState({
-          users: res.data,
+          users: res.data.items,
           loading: false
         })
       );
+  };
+
+  clearUsers = () => {
+    this.setState({
+      users: [],
+      loading: false
+    });
+  };
+
+  setAlert = (msg, type) => {
+    this.setState({alert: {msg: msg, type: type}})
+    setTimeout(() => this.setState({ alert: null }), 5000)
   }
 
   render() {
@@ -31,7 +44,13 @@ class App extends Component {
       <div>
         <Navbar />
         <div className="container">
-          <Search />
+          <Alert alert={this.state.alert}/>
+          <Search
+            searchUsers={this.searchUsers}
+            clearUsers={this.clearUsers}
+            showClear={this.state.users.length > 0 ? true : false}
+            setAlert={this.setAlert}
+          />
           <User loading={this.state.loading} users={this.state.users} />
         </div>
       </div>
--- github-finder/src/components/users/Search.js
+++ github-finder/src/components/users/Search.js
@@ -13,7 +13,12 @@ class Search extends Component {
 
   onSubmit = (e) => {
     e.preventDefault();
-    
+    if(this.state.text === '') {
+      this.props.setAlert('Please enter something', 'light')
+    } else {
+      this.props.searchUsers(this.state.text)
+      this.setState({ text: '' })
+    }
   }
 
   render() {
@@ -33,6 +38,8 @@ class Search extends Component {
             className="btn btn-dark btn-block"
           />
         </form>
+        {this.props.showClear && <button className="btn btn-light btn-block" onClick={this.props.clearUsers}>Clear</button> }
+        
       </div>
     );
   }
