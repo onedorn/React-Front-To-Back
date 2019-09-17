97ae3481435bce44b4b7a0e0d7f387da8217db04
Volodymyr Syvoraksha <vpsyvoraksha@gmail.com>
2019-09-18 00:06:58 +0300

    Create redux store

--- it-logger/src/App.js
+++ it-logger/src/App.js
@@ -4,6 +4,8 @@ import Logs from "./components/logs/Logs";
 import AddBtn from "./components/layout/AddBtn";
 import AddLogModal from "./components/logs/AddLogModal";
 import EditLogModal from "./components/logs/EditLogModal";
+import { Provider } from "react-redux";
+import store from "./store";
 
 import "materialize-css/dist/css/materialize.min.css";
 import M from "materialize-css/dist/js/materialize.min.js";
@@ -18,17 +20,19 @@ const App = () => {
   });
 
   return (
-    <div className="App">
-      <SearchBar />
-      <div className="container">
-        <AddBtn />
-        <AddLogModal />
-        <EditLogModal />
-        <AddTechModal />
-        <TechListModal />
-        <Logs />
+    <Provider store={store}>
+      <div className="App">
+        <SearchBar />
+        <div className="container">
+          <AddBtn />
+          <AddLogModal />
+          <EditLogModal />
+          <AddTechModal />
+          <TechListModal />
+          <Logs />
+        </div>
       </div>
-    </div>
+    </Provider>
   );
 };
 
